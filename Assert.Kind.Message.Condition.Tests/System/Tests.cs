ad6fc0ceaf7ac72ef270f9ece56f69aebb53b846
Denis <dddenisss35@gmail.com>
2024-05-23 13:44:41 +0300

    Readme

--- Assert.Kind.Message.Condition.Tests/System/Tests.cs
+++ Assert.Kind.Message.Condition.Tests/System/Tests.cs
@@ -8,15 +8,16 @@ public class Tests {
 
     [Test]
     public void Test_00_Exceptions() {
+        // Exceptions.Argument
         NUnit.Framework.Assert.Catch<ArgumentException>( () => throw Exceptions.Argument.ArgumentException( $"ArgumentException" ) );
         NUnit.Framework.Assert.Catch<ArgumentOutOfRangeException>( () => throw Exceptions.Argument.ArgumentOutOfRangeException( $"ArgumentOutOfRangeException" ) );
         NUnit.Framework.Assert.Catch<ArgumentNullException>( () => throw Exceptions.Argument.ArgumentNullException( $"ArgumentNullException" ) );
-
+        // Exceptions.Operation
         NUnit.Framework.Assert.Catch<InvalidOperationException>( () => throw Exceptions.Operation.InvalidOperationException( $"InvalidOperationException" ) );
-
+        // Exceptions.Object
         NUnit.Framework.Assert.Catch<ObjectNotReadyException>( () => throw Exceptions.Object.ObjectNotReadyException( $"ObjectNotReadyException" ) );
         NUnit.Framework.Assert.Catch<ObjectDisposedException>( () => throw Exceptions.Object.ObjectDisposedException( $"ObjectDisposedException" ) );
-
+        // Exceptions.Internal
         NUnit.Framework.Assert.Catch<Exception>( () => throw Exceptions.Internal.Exception( $"Exception" ) );
         NUnit.Framework.Assert.Catch<NullReferenceException>( () => throw Exceptions.Internal.NullReference( $"NullReferenceException" ) );
         NUnit.Framework.Assert.Catch<NotSupportedException>( () => throw Exceptions.Internal.NotSupported( $"NotSupportedException" ) );
@@ -25,10 +26,13 @@ public class Tests {
 
     [Test]
     public void Test_01_Assertions() {
+        // Assert.Argument
         NUnit.Framework.Assert.Catch<ArgumentException>( () => Assert.Argument.Message( $"ArgumentException" ).Valid( false ) );
         NUnit.Framework.Assert.Catch<ArgumentOutOfRangeException>( () => Assert.Argument.Message( $"ArgumentOutOfRangeException" ).InRange( false ) );
         NUnit.Framework.Assert.Catch<ArgumentNullException>( () => Assert.Argument.Message( $"ArgumentNullException" ).NotNull( false ) );
+        // Assert.Operation
         NUnit.Framework.Assert.Catch<InvalidOperationException>( () => Assert.Operation.Message( $"InvalidOperationException" ).Valid( false ) );
+        // Assert.Object
         NUnit.Framework.Assert.Catch<ObjectNotReadyException>( () => Assert.Object.Message( $"ObjectNotReadyException" ).Ready( false ) );
         NUnit.Framework.Assert.Catch<ObjectDisposedException>( () => Assert.Object.Message( $"ObjectDisposedException" ).NotDisposed( false ) );
     }
--- Readme.md
+++ Readme.md
@@ -1,16 +1,38 @@
 # Overview
-The assertion library with a very convenient fluent interface.
+The assertions library with a very convenient fluent interface.
 
 ## Assertions Example
-- ```Assert.Argument.Message( $"ArgumentException" ).Valid( ... )```
-- ```Assert.Argument.Message( $"ArgumentOutOfRangeException" ).InRange( ... )```
-- ```Assert.Argument.Message( $"ArgumentNullException" ).NotNull( ... )```
-- ```Assert.Operation.Message( $"InvalidOperationException" ).Valid( ... )```
-- ```Assert.Object.Message( $"ObjectNotInitializedException" ).Initialized( ... )```
-- ```Assert.Object.Message( $"ObjectDisposedException" ).Alive( ... )```
+```
+// Assert.Argument
+Assert.Argument.Message( $"ArgumentException" ).Valid( ... )
+Assert.Argument.Message( $"ArgumentOutOfRangeException" ).InRange( ... )
+Assert.Argument.Message( $"ArgumentNullException" ).NotNull( ... )
+
+// Assert.Operation
+Assert.Operation.Message( $"InvalidOperationException" ).Valid( ... )
+
+// Assert.Object
+Assert.Object.Message( $"ObjectNotReadyException" ).Ready( ... )
+Assert.Object.Message( $"ObjectDisposedException" ).NotDisposed( ... )
+```
 
 ## Exceptions Example
-- ```throw Exceptions.Internal.Exception( $"Exception" )```
-- ```throw Exceptions.Internal.NullReference( $"NullReferenceException" )```
-- ```throw Exceptions.Internal.NotSupported( $"NotSupportedException" )```
-- ```throw Exceptions.Internal.NotImplemented( $"NotImplementedException" )```
+```
+// Exceptions.Argument
+throw Exceptions.Argument.ArgumentException( $"ArgumentException" )
+throw Exceptions.Argument.ArgumentOutOfRangeException( $"ArgumentOutOfRangeException" )
+throw Exceptions.Argument.ArgumentNullException( $"ArgumentNullException" )
+
+// Exceptions.Operation
+throw Exceptions.Operation.InvalidOperationException( $"InvalidOperationException" )
+
+// Exceptions.Object
+throw Exceptions.Object.ObjectNotReadyException( $"ObjectNotReadyException" )
+throw Exceptions.Object.ObjectDisposedException( $"ObjectDisposedException" )
+
+// Exceptions.Internal
+throw Exceptions.Internal.Exception( $"Exception" )
+throw Exceptions.Internal.NullReference( $"NullReferenceException" )
+throw Exceptions.Internal.NotSupported( $"NotSupportedException" )
+throw Exceptions.Internal.NotImplemented( $"NotImplementedException" )
+```
